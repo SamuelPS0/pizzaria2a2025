2dec99c7eb434ad03025bb431997c60028143029
SamuelPS0 <rm93980@estudante.fieb.edu.br>
2025-09-15 12:27:07 -0300

    Inicio da instalação do AXIOS

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,4 +1,22 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
 const Produtos = () => {
+
+    const [dados, setDados] = useState ([]);
+    const pegarPizzas = () => {
+        axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
+        .then ((response) => setDados(response.data))
+        .catch((error) => console.log(error))
+    }
+
+    useEffect(()=>{
+        pegarPizzas();
+    }, [])
+
+    useEffect(() =>{
+        alert(dados)
+    }, [dados])
 
     // Objeto de produtos da lista
     const pizzas = [
@@ -7,7 +25,7 @@ const Produtos = () => {
         'Pizza Baiana',
         'Pizza Portuguesa',
         'Pizza de Frango'
-        
+
     ]
 
     const listaPizzas = pizzas.map(pizza => <li>{pizza}</li>);
